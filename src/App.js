b3e7e250df3303455f1f5334726add088c259e7e
Saurabh Garg <saurabh.garg18590@gmail.com>
2021-02-20 21:28:31 -0500

    feat: Boilerplate chat application added by Chat Engine

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import {ChatEngine} from 'react-chat-engine'
+
+import './App.css';
+
+const App = () => {
+    return (
+        <ChatEngine 
+            height="100vh"
+            projectID="0c04ad8e-094f-4fd6-a4eb-8978fc365eb5"
+            userName="saurabhgarg"
+            userSecret="123123"   
+        />
+    );
+}
+
+export default App;
